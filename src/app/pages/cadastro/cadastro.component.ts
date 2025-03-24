11f5184c06f87356197aa7d8d2a6dbc0e7861ae3
Alessandro Tulioli <alessandro.tulioli@csu.com.br>
2025-03-24 20:55:54 -0300

    Cria componente formulário e rotas

---
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormularioComponent } from "../../componentes/formulario/formulario.component";
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [FormularioComponent],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.css'
+})
+export class CadastroComponent {
+
+}
